dc548f1f10f14289d83d48580f4931b8399bd6c9
Adriano rsvp.github.com <rsvp@users.noreply.github.com>
2018-04-07 08:48:47 -0700

    test_4pytest.py: include dummy doctest
    pytest command in .travis.yml includes --doctest-module

--- test_4pytest.py
+++ test_4pytest.py
@@ -1,4 +1,4 @@
-#  Python Module for import                           Date : 2018-04-06
+#  Python Module for import                           Date : 2018-04-07
 #  vim: set fileencoding=utf-8 ff=unix tw=78 ai syn=python : per Python PEP 0263 
 ''' 
 _______________|  test_4pytest : Test pytest within integration like Travis.
@@ -10,6 +10,7 @@ REFERENCE:
                   or PDF at http://pytest.org/latest/pytest.pdf
 
 CHANGE LOG  
+2018-04-07  Add fadd and its doctest.
 2018-04-06  First version to dummy testing.
 '''
 
@@ -19,6 +20,19 @@ from __future__ import absolute_import, print_function
 def test_assert_try_repo():
     '''Dummy test.'''
     assert "Hello" == "Hello"
+
+
+def fadd( x, y ):
+    '''Simple fuction.'''
+    return x+y
+
+
+def test_fadd_try_repo():
+    '''Test fadd as doctest.
+    >>> fadd(2, 3)
+    5
+    '''
+    pass 
 
 
 if __name__ == "__main__":
